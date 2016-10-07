d8fc08075f308ea9d9e151c8cafb46c2f2726975
Irsyad Rahman <irsyadrahman78@gmail.com>
2016-10-07 21:31:06 +0700

    starActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -1,17 +1,33 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.annotation.TargetApi;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HasilActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR,0);
+
+        int yearNow = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " Lahir pada tahun "+ tahunLahir);
+
+        setTitle("Hasil");
     }
 
     @Override
